1966a94dd9a4b39c25d384e0efdb188f0129f6f2
Luiyi Valentin <luiyivalentin18@gmail.com>
2025-08-16 13:14:16 -0400

    Modify setup

--- dist/index.js
+++ dist/index.js
@@ -27609,11 +27609,13 @@ async function run() {
 }
 
 async function setupKtlint(version) {
-    const ktlintInstall = `curl -sSLO https://github.com/pinterest/ktlint/releases/download/${version}/ktlint && chmod a+x ktlint && sudo mv ktlint /usr/local/bin/`
+    // const ktlintInstall = `curl -sSLO https://github.com/pinterest/ktlint/releases/download/${version}/ktlint && chmod a+x ktlint && sudo mv ktlint /usr/local/bin/`
 
     core.info(`Installing Ktlint version: ${version}...`);
 
-    await exec.exec(ktlintInstall);
+    await exec.exec('curl', '-sSLO', `https://github.com/pinterest/ktlint/releases/download/${version}/ktlint`);
+    await exec.exec('chmod', 'a+x', 'ktlint');
+    await exec.exec('mv', 'ktlint', '/usr/local/bin/');
 
     core.info("Ktlint installed successfully.");async 
 }
